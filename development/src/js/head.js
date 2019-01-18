5eacbdda3d5b7e698c3fd41a5bcdf3d3a232b080
Alex Hudson <ahudson@alexhudsondesign.com>
2019-01-17 21:06:12 -0800

    Added webpack-stream to devDependencies; built JS

--- development/src/js/head.js
+++ development/src/js/head.js
@@ -1,8 +1,6 @@
 var foo = require('./_modules/foo.js');
 var bar = require('./_modules/bar.js');
 
-console.log(foo.foo(5));
-console.log(bar.bar(5));
 const joe = () => console.log('babel transpile working');
 
 joe();
