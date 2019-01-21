aaeff2cdd12409918322530ed6ef68f0c5a6e3c2
Alex Hudson <ahudson@alexhudsondesign.com>
2019-01-20 16:01:15 -0800

    Created Drift integration module; commented from head.js for now

--- development/src/js/head.js
+++ development/src/js/head.js
@@ -1,5 +1,6 @@
 var foo = require('./_modules/foo.js');
 var bar = require('./_modules/bar.js');
+// var drift = require('./_modules/drift.js');
 
 const joe = () => console.log('babel transpile working');
 
