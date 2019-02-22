09c298fe24047d8e314d38cb38f7e776efa148ea
Alex Hudson <ahudson@alexhudsondesign.com>
2019-02-22 15:58:13 -0800

    Disabled drift

--- development/src/js/head.js
+++ development/src/js/head.js
@@ -1,5 +1,5 @@
 const Modernizr = require('modernizr');
-const Drift = require('./_modules/drift.js');
+// const Drift = require('./_modules/drift.js');
 
 // if (Modernizr.flexbox) {
 //   console.log('Test to show Modernizr is working. This site has flexbox.');
